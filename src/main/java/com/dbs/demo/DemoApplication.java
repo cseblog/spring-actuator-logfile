0d6383ad23f5c72dae4b8908b4198b2804b39a09
Trung <trunghuynh.cse@gmail.com>
2020-05-01 16:57:56 +0800

    upadate demo log with actuator

---
+++ src/main/java/com/dbs/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.dbs.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		while ( true ) {
+			//log4j here
+			log.info("Testing...{}", System.currentTimeMillis());
+			Thread.sleep(60);
+		}
+	}
+}
